8b9188248b93e2aec557442d5d78d5f545d08bbc
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-16 16:10:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,22 +15,65 @@ function render() {
        }
        // Remove the trailing comma and space
        listInfoStr = listInfoStr.slice(0, -2);
-       arrayContainer.textContent = `{${listInfoStr}}`
+       arrayContainer.textContent = `{${listInfoStr}}`;
 };
 
 render();
 
 /*Displays "You need a $100 to buy a dog." under buttons. When you earn money, per each $100, you buy 1 dog and it updates dogs array and decreases money buy $ amount used*/
 
+const buyDogButton = document.querySelector(".buyDog")
+const display = document.querySelector("#display")
+
+listInfo.dogs = 0;
+
+buyDogButton.addEventListener("click", function() {
+       if (listInfo.money >= 100) {
+           listInfo.money -= 100;
+           listInfo.dogs += 1;
+           render();
+       } else {
+           display.textContent = "You need $100 to buy a dog.";
+       }
+   });
+
+
 /*Displays "You don't have a dog, it couldn't of died." After you buy a dog, the dogs array decreases by 1*/
+const deathButton = document.querySelector(".death");
+
+deathButton.addEventListener("click", function() {
+       if (listInfo.dogs > 0) {
+           listInfo.dogs -= 1;
+           render();
+       } else {
+           display.textContent = "You don't have a dog, it couldn't have died.";
+       }
+   });
 
 // Adds $100 each click to the money array
-       const moneyButton = document.querySelector(".money");
-       moneyButton.textContent = "name: " + listInfo.name;
-       moneyButton.appendChild(money);
+const workButton = document.querySelector(".work");
+
+listInfo.money = 0;
+
+workButton.addEventListener("click", function() {
+       listInfo.money += 100;
+       render();
+   });
 
 /* Displays "You need $1000 to buy a laptop." under button. When you earn money, per each $1000, you buy 1 laptop and it updates laptops array and decreases money buy $ amount used*/
+const laptopButton = document.querySelector(".laptop");
 
+listInfo.laptops = 0;
+
+laptopButton.addEventListener("click", function() {
+       if (listInfo.money >= 1000) {
+           listInfo.money -= 1000;
+           listInfo.laptops += 1;
+           render();
+       } else {
+           display.textContent = "You need $1000 to buy a laptop.";
+       }
+   });
 
 
        
